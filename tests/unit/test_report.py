8197b2c044a985354fbcb55e66a436ebcc522c0f
Tony Meyer <tony@spamexperts.com>
2016-09-29 23:36:08 +1300

    Fix test.
     - Change the IP addresses that are used, as the old ones are
    considered private by ipaddress (although not by ipaddr).
     - Mock time.time() and random.randint() so that we can properly
    compare without having to patch out bits of the report.

--- tests/unit/test_report.py
+++ tests/unit/test_report.py
@@ -4,6 +4,8 @@ from __future__ import print_function
 
 import unittest
 
+import mock
+
 from rps.report import IPEvent
 from rps.report import IPv4Events
 from rps.report import IPv6Events
@@ -14,27 +16,34 @@ from rps.report import RepeatedIPv4Events
 
 # XXX These should be reformatted to proper unittests
 class TestReport(unittest.TestCase):
+    def setUp(self):
+        """Prepare for a single test."""
+        mock.patch("random.randint", return_value=1).start()
+        mock.patch("time.time", return_value=1156727880.0).start()
+
+    def tearDown(self):
+        """Clean up after a single test."""
+        mock.patch.stopall()
+
     def test_report(self):
-        """Check that the example in the specification generates the correct
-        report data."""
+        """Check that the example in the specification generates the
+        correct report data."""
         # The report is from the user "dfs" with password "foo".
         subreports = [
-            IPv4Events([IPEvent("192.0.2.2", "AUTO-SPAM"),
-                        IPEvent("192.0.2.3", "GREYLISTED")]),
+            IPv4Events([IPEvent("5.79.73.204", "AUTO-SPAM"),
+                        IPEvent("5.79.65.71", "GREYLISTED")]),
             RepeatedIPv4Events([
-                RepeatedIPEvent("192.0.2.4", "INVALID-RECIPIENT", 3)]),
+                RepeatedIPEvent("93.184.216.34", "INVALID-RECIPIENT", 3)]),
             IPv6Events([
-                IPEvent("2001:db8:1d:e4:2e0:18ff:feab:147f",
+                IPEvent("2606:2800:220:1:248:1893:25c8:1946",
                         "VALID-RECIPIENT")]),
         ]
         report = ReportClient.generate_report(subreports, "dfs", "foo")
-        correct = ("02036466732a9a82d6512964f74bd9daeb01000ac000020203c00002030"
-                   "1030006c0000204080302001120010db8001d00e402e018fffeab147f07"
-                   "9c7dcb30a60592faa3a8")
+        correct = ("0203646673010101010101010144f2444801000a054f49cc030"
+                   "54f4147010300065db8d8220803020011260628000220000102"
+                   "48189325c81946070c90ebfd8d9da4a67577")
         hex_report = "".join("%.2x" % ord(c) for c in report)
-        # Make the random bytes and timestamp the same.
-        correct = correct[:10] + hex_report[10:34] + correct[34:]
         # XXX Why are we printing things in a unit test?
         print(hex_report[:-20])
         print(correct[:-20])
-        self.assertEqual(hex_report[:-20], correct[:-20])
+        self.assertEqual(hex_report, correct)
