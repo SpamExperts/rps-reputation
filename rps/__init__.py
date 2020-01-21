f0812ddec96e997ffea212c24b1a39a393eed135
Silviu Surcica <silviu.surcica@solarwinds.com>
2020-01-21 14:13:06 +0200

    get package version

--- rps/__init__.py
+++ rps/__init__.py
@@ -1,1 +1,5 @@
 """An implementation of the Roaring Penguin IP reputation reporting system."""
+import yaml
+
+with open('manifest.yml', 'r') as f:
+    __version__ = yaml.safe_load(f).get('version')
